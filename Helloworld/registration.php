e8b53da49f19395aa3fd4acd30ea79293e2270e4
rishikeshgdev <mystyle73019@gmail.com>
2022-12-04 14:28:09 +0530

    test

---
+++ Helloworld/registration.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @author 18th DigiTech Team
+ * @copyright Copyright (c) 2022 18th DigiTech (https://www.18thdigitech.com)
+ * @package RKG_Helloworld
+ */
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'RKG_Helloworld',
+    __DIR__
+);
